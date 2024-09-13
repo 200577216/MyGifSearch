3aff0548f996ea5ef49e2f533f37e569cebd601a
Jaydutt-patel <jaydutt1998@gmail.com>
2024-09-13 15:38:00 -0400

    Feat: clean code | add footer | CSS changes on hover

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 const searchButton = document.getElementById("searchButton");
 const gifContainer = document.getElementById("gifContainer");
-let isSearchActive = true; // To track whether the button should be in "Search" or "Clear" mode
+let isSearchActive = true;
 
 searchButton.addEventListener("click", () => {
   if (isSearchActive) {
-    // If it's in "Search" mode
     const searchQuery = document.getElementById("searchInput").value;
     if (searchQuery.trim() === "") {
-      alert("Please enter a search term");
-      return; // Exit if input is empty
+      alert("Your search is empty!!!");
+      return;
     }
 
-    const apiKey = "dmPUZGuKrZyDWGwHFjlkh0Kjt5qA1zGX"; // Replace with your Giphy API key
+    const apiKey = "dmPUZGuKrZyDWGwHFjlkh0Kjt5qA1zGX";
     const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchQuery}&limit=10`;
 
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        gifContainer.innerHTML = ""; // Clear previous GIFs
+        gifContainer.innerHTML = "";
         data.data.forEach((gif) => {
           const colDiv = document.createElement("div");
           colDiv.classList.add("col-md-3");
@@ -34,20 +33,17 @@ searchButton.addEventListener("click", () => {
 
         // Update the button to "Clear" and change its color to yellow
         searchButton.textContent = "Clear";
-        searchButton.classList.remove("btn-primary"); // Remove the primary color class
-        searchButton.classList.add("btn-warning"); // Add the warning class for yellow button
+        searchButton.classList.remove("btn-primary");
+        searchButton.classList.add("btn-warning");
         isSearchActive = false;
       })
       .catch((error) => console.error("Error fetching GIFs:", error));
   } else {
-    // If it's in "Clear" mode
     gifContainer.innerHTML = ""; // Clear GIFs
-    document.getElementById("searchInput").value = ""; // Clear the input field
-
-    // Revert the button text to "Search" and change its color back to blue
+    document.getElementById("searchInput").value = "";
     searchButton.textContent = "Search";
-    searchButton.classList.remove("btn-warning"); // Remove the warning (yellow) class
-    searchButton.classList.add("btn-primary"); // Add the primary class for blue button
+    searchButton.classList.remove("btn-warning");
+    searchButton.classList.add("btn-primary");
     isSearchActive = true;
   }
 });
